5c75a0d16d32835da5c6664c183b8cb05acea46b
Ronaldo Torre <ronaldo.torre@outlook.com>
2018-11-08 10:21:32 -0200

    Correção na configuração de acesso ao mongodb, incluir tratamento de erro.

--- storeApi/src/config/database.js
+++ storeApi/src/config/database.js
@@ -7,9 +7,31 @@
  * ---------------------------------------------------------*/
 
 const mongoose = require('mongoose');
+const datatype = "mongodb://";
+const database = "/storeapp";
 const host = "localhost";
 
-mongoose.connect('mongodb://'+host+'/storeapp', { useMongoClient: true });
+const options = {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    autoIndex: false,
+    reconnectTries: Number.MAX_VALUE,
+    reconnectInterval: 500,
+    poolSize: 10,
+    bufferMaxEntries: 0,
+    connectTimeoutMS: 10000,
+    socketTimeoutMS: 45000,
+    family: 4
+}
+
+//mongoose.connect('mongodb://'+host+'/storeapp', { useMongoClient: true });
+mongoose.connect(datatype+host+database, options, function(error){
+    if(error)
+        console.log("Ocurred error! "+error);
+    else
+        console.log("Mongodb connected in environment "+host+"...");
+});
 mongoose.Promise = global.Promise;
 mongoose.pluralize(null);
 
