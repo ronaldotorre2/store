89c7a7bc32b129b0969386cbd7cdb9d4a0caa31d
Ronaldo Torre <ronaldo.torre@outlook.com>
2018-11-26 16:17:23 -0200

    StoreWeb: Incluir componente login.

--- storeWeb/src/app/app.module.ts
+++ storeWeb/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { Error404Component } from './pages/error404/error404.component';
 import { ContactComponent } from './pages/contact/contact.component';
+import { LoginComponent } from './pages/login/login.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { ContactComponent } from './pages/contact/contact.component';
     HomeComponent,
     AboutComponent,
     Error404Component,
-    ContactComponent
+    ContactComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
